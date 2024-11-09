d877de3c8ff963301c8b0f8955dd89062d4edf74
Lor <lorelailyons@protonmail.com>
2024-11-09 00:00:23 -0500

    Cleaning code and refining comments.

--- src/tic_tac_two/main.clj
+++ src/tic_tac_two/main.clj
@@ -11,19 +11,14 @@
    [" " " " " "]
    [" " " " " "]])
 
-(defn reset-game []
-  {:board [[" " " " " "] [" " " " " "] [" " " " " "]]
-   :moves-made 0})
-
 (defn legal-move? [row col board]
   (clojure.string/blank? (get-in board [row col])))
 
 (defn check-winner
-  "This part of the algorithm reviews the state of the board
-   specifically by evaluating the diagnal axis of the board
-   and determining whether there are any filled lines in one
-   cardinal direction. If so it will return that particular 
-   array or nil"
+  "This part of the algorithm reviews the state of the board, iterating through
+    all rows, and finally the diagnal axis of the board, determining whether 
+    there are any filled lines across any one of 8 axis. If so it will return
+    that particular array or nil"
   [board]
   (let [diag1 [(get-in board [0 0]) (get-in board [1 1]) (get-in board [2 2])]
         diag2 [(get-in board [0 2]) (get-in board [1 1]) (get-in board [2 0])]
@@ -37,22 +32,18 @@
           all-lines)))
 
 (defn make-move
-  "Make-move functions by taking appropriate
-   symbol ('X' or 'O') into the appropriate
-   spot in the state vectors for both the
-   player and computer."
+  "Make-move functions by taking appropriate symbol ('X' or 'O') into the 
+    appropriate spot in the state vectors for both the player and computer."
   [board row col player]
   (if (legal-move? row col board)
    (assoc-in board [row col] player)
    (do
      (println "Invalid move! Spot already taken.")
      board)))
-; ex. =>([0 1] [0 2] [1 0] [1 1] [1 2] [2 0] [2 1])
 
 (defn get-available-moves
-  "Iterate through the 'open' board positions and
-   return a destructured array of available positions
-   based on their grid coordinates."
+  "Iterate through the 'open' board positions and return a destructured array
+    of available positions based on their grid coordinates."
   [board-state]
   (->> (for [row (range (count board-state))
              col (range (count (first board-state)))]
@@ -61,10 +52,9 @@
        (remove nil?)))
 
 (defn evaluate-board
-  "This function applies a score to the potential move
-   being evaluated. A score of 0 inevitably continues
-   the game and will lead to an updated board state,
-   while a 1 or -1 signal the end of the game."
+  "This function applies a score to the potential move being evaluated. A score
+    of 0 inevitably continues the game and will lead to an updated board state,
+    while a 1 or -1 signal the end of the game."
   [board player]
   (cond
     (= (check-winner board) player) 1
@@ -73,10 +63,9 @@
     :else 0))
 
 (defn minimax
-  "Minimax is a well known recursive algorithm that evaluates a 
-    possible set of moves and applies scores of either 0 to each 
-    one,indicating the game will progress, or otherwise declaring
-    a winner."
+  "Minimax is a well known recursive algorithm that evaluates a possible set of 
+    moves and applies scores of either 0 to each one,indicating the game will 
+    progress, or otherwise declaring a winner."
   [board player depth]
   (let [winner (check-winner board)]
    (if (or (zero? depth)
@@ -98,11 +87,10 @@
         (some #(= (Integer/parseInt input) %) [0 1 2]))
    (catch NumberFormatException _ false)))
 
-(defn get-valid-input
-  "This function ensures that the user can only enter an integers [0 1 2]"
+(defn get-valid-input 
   [prompt valid-fn]
-  (loop []
-    (println prompt)
+  (loop [] ;continuously prompt user input until valid entry
+    (println prompt) ;pls
     (let [input (read-line)]
       (if (valid-fn input)
         (Integer/parseInt input)
@@ -110,7 +98,15 @@
           (println "Invalid input. Please try again.")
           (recur))))))
 
-(defn play-game [player-name stats]
+(defn play-game
+  "This is the main game loop. Since the program is optimized/unlosable for the
+    computer, technically only a draw or a loss may occur, but not by design - 
+    rather the algorithm ensures that the computer plays 'optimally' so 
+    theoretically the player actually could win; but the algorithm ensures at 
+    least a draw as long as the computer follows the code without unforeseen 
+    error. Accompanied are tests, and any further testing is welcome and I can 
+    provide further testing upon request."
+  [player-name stats]
   (loop [current-board board-state
          moves-made 0
          current-stats stats]
@@ -118,7 +114,7 @@
     (let [winner (check-winner current-board)]
       (cond
         winner
-        (let [is-player-win? (= winner "X")
+        (let [is-player-win? (= winner "X");loss condition or 'win' if win was possible.
               new-stats (console/update-stats current-stats (if is-player-win? :win :loss))]
           (console/print-draw-or-computer-win false is-player-win? player-name)
           (console/print-stats new-stats 0)
@@ -126,7 +122,7 @@
             (recur board-state 0 new-stats)
             new-stats))
 
-        (>= moves-made 9)
+        (>= moves-made 9);draw condition
         (let [new-stats (console/update-stats current-stats :draw)]
           (console/print-draw-or-computer-win true false player-name)
           (console/print-stats new-stats 0)
@@ -134,7 +130,7 @@
             (recur board-state 0 new-stats)
             new-stats))
 
-        :else
+        :else;if not a loss or draw, the game continues on and the computer takes a turn
         (let [row (get-valid-input "Enter your row [0, 1, 2]:" valid-input?)
               col (get-valid-input "Enter your column [0, 1, 2]:" valid-input?)
               board-after-player-move (make-move current-board row col "X")]
@@ -148,8 +144,11 @@
                   (let [board-after-computer-move (make-move board-after-player-move ai-row ai-col "O")]
                     (recur board-after-computer-move (+ moves-made 2) current-stats)))))))))))
 
-
 (defn -main
+    "The entry point to the program. It first prints a heading and then takes
+      user input simply for their name. After welcoming the player, it will
+      invoke the main game loop. The game will exit and print the final stats
+      when the player has chosen not to continue playing."
   []
   (console/print-heading) 
   (let [player-name (read-line)
